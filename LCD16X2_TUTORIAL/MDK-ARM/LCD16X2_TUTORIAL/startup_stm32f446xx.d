2f3975adf3767d84f951acfdce8db9de49c5346e
ENG_Edison <edisonngunjiri@gmail.com>
2021-07-25 13:24:00 +0300

    This code is for interfacing lcd16x2
    It is configured using 4bit mode

---
+++ LCD16X2_TUTORIAL/MDK-ARM/LCD16X2_TUTORIAL/startup_stm32f446xx.d
@@ -0,0 +1,1 @@
+lcd16x2_tutorial\startup_stm32f446xx.o: startup_stm32f446xx.s
---
+++ LCD16X2_TUTORIAL/MDK-ARM/RTE/_LCD16X2_TUTORIAL/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LCD16X2_TUTORIAL' 
+ * Target:  'LCD16X2_TUTORIAL' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
