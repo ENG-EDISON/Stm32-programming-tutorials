16d95105ed85eeeb5e5a41e0db5c6a5b2877a0b4
ENG_Edison <edisonngunjiri@gmail.com>
2021-11-28 10:51:05 +0300

    CAN BUS EXAMPLE
    Add the hal driver directory to the main folder when running on keil

---
+++ CAN_BUS/MDK-ARM/RTE/_TRY_CAN_BUS/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TRY_CAN_BUS' 
+ * Target:  'TRY_CAN_BUS' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ CAN_BUS/MDK-ARM/TRY_CAN_BUS/startup_stm32f446xx.d
@@ -0,0 +1,1 @@
+try_can_bus\startup_stm32f446xx.o: startup_stm32f446xx.s
